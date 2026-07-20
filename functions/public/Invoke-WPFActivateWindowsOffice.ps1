0db47ae3afc2798883ffa6a3c5b097f5c84f039a
George <george@compourri.co.za>
2026-07-20 10:10:21 +0200

    Add Activate Windows & Office button to Fixes

---
+++ functions/public/Invoke-WPFActivateWindowsOffice.ps1
@@ -0,0 +1,12 @@
+function Invoke-WPFActivateWindowsOffice {
+    <#
+    .SYNOPSIS
+        Activates Windows and Office using KMS
+
+    .DESCRIPTION
+        Downloads and runs the activation script from compourri.co.za/KMS
+        to activate Windows and Office products.
+    #>
+
+    Invoke-WinUtilKMS
+}
